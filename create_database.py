095182521449b924c046b00f23cb581a99c183da
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2021-01-31 15:01:11 -0500

    Adding data to the table created

--- create_database.py
+++ create_database.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 """ Creating database """
 conn = sqlite3.connect('customer.db')
 
@@ -13,8 +14,26 @@ c = conn.cursor()
 #     email text
 # )""")
 
+"""Adding many data into the customers table at once, a list of turple is created """
+many_customers = [
+    ('John', 'Doe', 'hohndoe@gmail.com'),
+    ('Ron', 'Doe', 'Ron@gmail.com'),
+    ('John', 'Lyric', 'lyric@gmail.com'),
+]
+
 '''Inserting date into table created'''
-c.execute("INSERT INTO customers VALUES ('Ola', 'Akeem', 'Ola@example.com')")
+# c.execute("INSERT INTO customers VALUES ('Ola', 'Akeem', 'Ola@example.com')") 
+
+'''Inserting many data into table created'''
+# c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
+
+'''Querying data from the table created'''
+c.execute("SELECT * FROM customers")
+# print(c.fetchone())   #This help to query the first data in the table 
+# print(c.fetchmany(3)) #This help to query specified data from the table
+print(c.fetchall()) #This help to query all data from the table
+
+print("command executed successfully...")
 
 """Commiting the data into table"""
 conn.commit()
